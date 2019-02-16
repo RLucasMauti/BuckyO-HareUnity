c8657b552b36aae130a8eac5f80348f882250411
RLucasMauti <45114459+RLucasMauti@users.noreply.github.com>
2019-02-16 16:27:43 -0500

    Add files via upload

---
+++ PickUp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+
+    public bool points;
+    public bool life;
+    public bool maxLife;
+
+    public PlayerHealthManager playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = GameObject.Find("Player").GetComponent<PlayerHealthManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            if (life) playerHealth.lives++;
+            if (maxLife) playerHealth.currentHealth = playerHealth.maxHealth;
+            if (points) playerHealth.points += 3000;
+
+            this.gameObject.SetActive(false);
+        }
+    }
+}
